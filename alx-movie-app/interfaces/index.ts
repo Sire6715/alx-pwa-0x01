c9de2dad958f9902e1c2538bc856d19cff9cb586
Prince <uchepibe@gmail.com>
2025-06-28 20:58:48 +0100

    Added the movies pages to you project

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -3,6 +3,32 @@ export interface ComponentProps {
 }
 
 export interface ButtonProps {
-     title: string;
-     action?: () => void;
+  title: string;
+  action?: () => void;
+}
+
+export interface MovieProps {
+  id?: string;
+  title: string;
+  posterImage: string;
+  releaseYear: string;
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id?: string;
+  titleText: TitleText;
+  primaryImage: PrimaryImage;
+  releaseYear: ReleaseYear;
 }
--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+/* The code snippet you provided is configuring options for a Next.js application using TypeScript.
+Here's an explanation of the specific configuration options: */
   reactStrictMode: true,
+  images:{
+    domains: ['m.media-amazon.com']
+  }
 };
 
 export default nextConfig;
