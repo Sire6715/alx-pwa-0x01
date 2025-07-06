38447064a22d1d93cd9e2d95ffae4b0c8b22848a
Prince <uchepibe@gmail.com>
2025-07-06 16:45:58 +0100

    PWA configuration

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
-/* The code snippet you provided is configuring options for a Next.js application using TypeScript.
+  /* The code snippet you provided is configuring options for a Next.js application using TypeScript.
 Here's an explanation of the specific configuration options: */
   reactStrictMode: true,
-  images:{
-    domains: ['m.media-amazon.com']
-  }
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
 };
 
-export default nextConfig;
+export default withPWA({ ...nextConfig });
